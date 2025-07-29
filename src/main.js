8ebd90f5f7ea1d2d0c79fee8eb80c45043fb9a85
amir <amirtaki623@gmail.com>
2025-07-29 23:29:20 +0330

    function searchImage js project

--- src/main.js
+++ src/main.js
@@ -2,3 +2,12 @@ const  searchForm = document.getElementById("serach-form")
 const  searchBox = document.getElementById("serach-box")
 const  searchResult = document.getElementById("search-result")
 const  showMoreBtn = document.getElementById("show-more-btn")
+// Link: <https://api.unsplash.com/search/photos?page=1&query=office>; rel="first", <https://api.unsplash.com/search/photos?page=1&query=office>; rel="prev", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="last", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="next"
+// X-Ratelimit-Limit: 1000
+// X-Ratelimit-Remaining: 9994
+
+let keyword = ""
+let page = 1;
+async function searchImage () {
+    
+}
