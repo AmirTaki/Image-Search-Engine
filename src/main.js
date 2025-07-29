cd7b57f3f658d0ac3879a90cc345a90f86387e03
amir <amirtaki623@gmail.com>
2025-07-29 23:43:50 +0330

    function searchImage js project part 11

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ let keyword = ""
 let page = 1;
 async function searchImage () {keyword
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}$client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}$client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
@@ -26,6 +26,9 @@ async function searchImage () {keyword
         imageLink.appendChild(image);
         searchResult.appendChild(imageLink)
     })
+    showMoreBtn.style.display = "block";
+    
+
     
 }
 
