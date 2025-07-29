f832ab4330ef45fcef5a561d3d74cb9e1f005231
amir <amirtaki623@gmail.com>
2025-07-29 23:16:45 +0330

    querySelect js project

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const  searchForm = document.getElementById("serach-form")
+const  searchBox = document.getElementById("serach-box")
+const  searchResult = document.getElementById("search-result")
+const  showMoreBtn = document.getElementById("show-more-btn")
