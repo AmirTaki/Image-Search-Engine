c472182e406f4c4624b4a87fb346fecefbe0b923
amir <amirtaki623@gmail.com>
2025-07-29 23:31:24 +0330

    function searchImage js project part 2

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ const  searchForm = document.getElementById("serach-form")
 const  searchBox = document.getElementById("serach-box")
 const  searchResult = document.getElementById("search-result")
 const  showMoreBtn = document.getElementById("show-more-btn")
-// Link: <https://api.unsplash.com/search/photos?page=1&query=office>; rel="first", <https://api.unsplash.com/search/photos?page=1&query=office>; rel="prev", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="last", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="next"
+// Link: <>; rel="first", <https://api.unsplash.com/search/photos?page=1&query=office>; rel="prev", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="last", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="next"
 // X-Ratelimit-Limit: 1000
 // X-Ratelimit-Remaining: 9994
 
 let keyword = ""
 let page = 1;
-async function searchImage () {
-    
+async function searchImage () {keyword
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}`
 }
